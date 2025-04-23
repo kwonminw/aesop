8052362293e442a3fc3d17221d910a32574f3d2e
kwonminw <alsdn4499@gmail.com>
2025-04-23 16:38:50 +0900

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,46 @@
 import { Link } from "react-router-dom";
 import { Tabletmenu, HeadUtils, Mobileicon } from "./";
+import { useState } from "react";
 import Gnb from "./Gnb";
 import './Header.css';
 
-
 const Header = () => {
-    return(
-        <header id="header">
-            <div className="header_inner">
-                <h1 className="logo"><Link to=''><img src="/assets/images/img.png" alt="이솝"/></Link></h1>
-                    <div className="header_left">
-                        <Tabletmenu />
-                        <Gnb />
-                        <p className="search"><Link><img src="/assets/images/search-symbol.png" /></Link></p>
-                    </div>
-                    <div className="header_right">
-                        <Mobileicon />
-                        <HeadUtils />
-                    </div>
-            </div>
-        </header>
-    )
-}
+  const [isSearch, setIsSearch] = useState(false);
+
+  const handleSearchView = () => {
+    setIsSearch(!isSearch);
+  };
+
+  return (
+    <header id="header">
+      <div className="header_inner">
+        <h1 className="logo">
+          <Link to=''><img src="/assets/images/img.png" alt="이솝" /></Link>
+        </h1>
+        <div className="header_left">
+          <Tabletmenu />
+          <Gnb />
+          <div className="search">
+            <button onClick={handleSearchView} className="search_button">
+              <img src="/assets/images/search-symbol.png" alt="검색" />
+            </button>
+          </div>
+        </div>
+        <div className="header_right">
+          <Mobileicon handleSearchView={handleSearchView} />
+          <HeadUtils />
+        </div>
+      </div>
+      {isSearch && (
+        <div className="aesop_search_modal">
+          <div className="aesop_modal_content">
+            <input type="text" placeholder="검색어를 입력해주세요" className="aesop_input" />
+            <button onClick={handleSearchView} className="aesop_close">Close</button>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
--- src/components/header/rightmenu/Mobileicon.jsx
+++ src/components/header/rightmenu/Mobileicon.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { Header } from "..";
 import Side from "../Side";
 import './Mobileicon.css';
 
-const Mobileicon = () => {
+const Mobileicon = ({ handleSearchView }) => {
     const [isSide, setIsSide] = useState(false);
 
     const handleSideOpen = () => {
@@ -13,8 +14,16 @@ const Mobileicon = () => {
     return(
         <div className="mobileicon">
                 <ul>
-                    <li><Link to=''><img src="/assets/images/search-symbol.png" alt="검색"/></Link></li>
-                    <li><Link to=''><img src="/assets/images/heart.png" alt="찜목록"/></Link></li>
+                    <li>
+                        <button onClick={handleSearchView} className="mobile_search">
+                            <img src="/assets/images/search-symbol.png" alt="검색"/>
+                        </button>
+                    </li>
+                    <li>
+                        <Link to=''>
+                            <img src="/assets/images/heart.png" alt="찜목록"/>
+                        </Link>
+                    </li>
                     <li className="cart"><Link to=''>카트</Link></li>
                     <li className="button"><button onClick={handleSideOpen}><img src="/assets/images/menu.png" alt="사이드메뉴"/></button></li>
                 </ul>
